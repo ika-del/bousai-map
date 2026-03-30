36ceef432a611fbcb58d35117a720adfe9ee43bd
ika-del <i.ka@be-kan.co.jp>
2026-03-30 17:06:14 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-import { redirect } from "next/navigation";
 export default function Home() {
-  redirect("/map.html");
+  return (
+    <iframe
+      src="/map.html"
+      style={{ width: "100vw", height: "100vh", border: "none", display: "block" }}
+    />
+  );
 }
